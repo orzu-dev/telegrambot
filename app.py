e32e83a72f39b8c93c028b990543df1146f7fb34
orzu-dev <qambarovazarifa442@gmail.com>
2025-01-16 11:55:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ user_votes = {}
 app = Flask(__name__)
 
 # Webhook URL sozlamalari
-WEBHOOK_URL = f"https://telegrambot-1osn.onrender.com/{bot_token}"
+WEBHOOK_URL = f"https://telegrambot.koyeb.app/{bot_token}"
+
 
 # Webhookni o'rnatish
 bot.remove_webhook()
